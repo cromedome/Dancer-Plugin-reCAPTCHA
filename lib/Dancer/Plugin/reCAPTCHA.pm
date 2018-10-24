646225cf45f83096a258df6e40523477240686b8
Jason A. Crome <jcrome@empoweredbenefits.com>
2018-10-24 15:33:08 -0400

    Various documentation changes.

--- lib/Dancer/Plugin/reCAPTCHA.pm
+++ lib/Dancer/Plugin/reCAPTCHA.pm
@@ -6,9 +6,19 @@ package Dancer::Plugin::reCAPTCHA;
 
 =head1 SYNOPSIS
 
+    # In your config.yml 
+    plugins: 
+        reCAPTCHA: 
+            public_key: "public key goes here" 
+            private_key: "private key goes here" 
+            theme: "dark"
+            type: "image"
+            size: "normal"
+
+    # In your Dancer app...
     use Dancer::Plugin::reCAPTCHA;
 
-    # In your form display....
+    # In your form display route...
     return template 'accounts/create', { 
         recaptcha => recaptcha_display(),
     };
@@ -18,9 +28,7 @@ package Dancer::Plugin::reCAPTCHA;
 
     # In your validation code....
     my $response  = param( 'recaptcha_response_field' );
-    my $result    = recaptcha_check(
-        $response,
-    );
+    my $result    = recaptcha_check( $response );
     die "User didn't match the CAPTCHA" unless $result->{ success };
 
 =cut
@@ -29,9 +37,9 @@ use Dancer ':syntax';
 use Dancer::Plugin;
 use Captcha::reCAPTCHA::V2;
 
-my $rc   = Captcha::reCAPTCHA::V2->new;
+my $rc = Captcha::reCAPTCHA::V2->new;
 
-=method recaptcha_display( )
+=method recaptcha_display()
 
 Generates the HTML needed to display the CAPTCHA.  This HTML is returned as
 a scalar value, and can easily be plugged into the template system of your 
@@ -58,7 +66,7 @@ register recaptcha_display => sub {
     );
 };
 
-=method recaptcha_check( $ )
+=method recaptcha_check()
 
 Verify that the value the user entered matches what's in the CAPTCHA.  This
 methods takes an argument that is the response string.
@@ -68,9 +76,7 @@ F< g-recaptcha-response >.
 For example:
 
     my $response  = param( 'g-recaptcha-response' );
-    my $result    = recaptcha_check(
-        $response
-    );
+    my $result    = recaptcha_check( $response );
     die "User didn't match the CAPTCHA" unless $result->{ success };
 
 See L<Captcha::reCAPTCHA::V2> for a description of the result hash.
@@ -90,6 +96,14 @@ register recaptcha_check => sub {
 =head2 TODO
 
 Add a real test suite.
+
+=head1 CREDITS
+
+The following people have contributes to C<Dancer::Plugin::reCAPTCHA> in some 
+way, either through bug reports, code, suggestions, or moral support:
+
+Mohammad S Anwar
+Shawn Sorichetti
 
 =head1 SEE ALSO
 
